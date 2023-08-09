f783d1b2139580092e026ec3a658aff94a3a0c0f
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2023-08-09 13:02:19 +0000

    initial commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import Helmet from 'helmet';
+import morgan from 'morgan';
