cdb1d2966808ac28d82d81432555ef9c8fffa8d1
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2023-08-27 14:12:11 +0000

    adding express files and depeendencies

--- server/index.js
+++ server/index.js
@@ -5,3 +5,31 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import Helmet from 'helmet';
 import morgan from 'morgan';
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/**
+ * CONFIGURATION
+ */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extende: false}))
+app.use(cors());
+
+/**
+ * ROUTES
+ */
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
