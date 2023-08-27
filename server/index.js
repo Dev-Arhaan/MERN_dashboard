70171a63bb8dc9fb1bd66b11dd80b259c621e22d
Arhaan Sayeed <dev.sayarhaan@gmail.com>
2023-08-27 14:51:48 +0000

    added mongodb

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import Helmet from 'helmet';
+import helmet from 'helmet';
 import morgan from 'morgan';
 
 import clientRoutes from "./routes/client.js";
@@ -20,9 +20,9 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"))
+app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extende: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /**
@@ -33,3 +33,16 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/**
+ * MONGOOSE SETUP
+ */
+
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
